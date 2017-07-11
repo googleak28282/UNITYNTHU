d156233cb8b1fecc74b18486563832e42bc2df69
googleak28282 <take890107@gmail.com>
2017-07-11 10:17:15 +0800

    new unity

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+    public GameObject target;
+    public float speed;
+    
+    // Use this for initialization
+    void Start () {
+            
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position != target.transform.position)
+            
+            transform.position = Vector3.MoveTowards(transform.position, target.transform.position, Time.deltaTime * speed);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    private int count;
+    public int wincount;
+    void Start () {
+        count = 0;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * moveVertical * speed / 2);
+        //transform.localPosition = new Vector3(transform.position.x + speed * moveHorizontal/50, transform.position.y, transform.position.z + speed * moveVertical / 50);
+        transform.Rotate(transform.up, moveHorizontal * Time.deltaTime * speed * 8);
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+
+            if (count == wincount) Time.timeScale = 0f;
+
+            float new_x,new_z;
+            new_x = Random.Range(-7.0f,7.0f);
+            new_z = Random.Range(-7.0f,7.0f);
+            
+            other.gameObject.transform.position = new Vector3(new_x,0.5f,new_z);
+            other.gameObject.SetActive(true);
+        }
+        if(other.gameObject.CompareTag("Enemy"))
+        {
+            Time.timeScale = 0f;
+        }
+    }
+}
